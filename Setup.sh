3b687a9650d360416fb87f3a941ec5551f612d98
Mark The Magnificent <mnegretev@gmail.com>
2020-01-02 22:31:17 -0600

    First Draft with hardware simulation, navigation and pocketsphinx

---
+++ Setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get install ros-melodic-tf2-bullet -y
+sudo apt-get install ros-melodic-map-server -y
+sudo apt-get install ros-melodic-amcl -y
+sudo apt-get install ros-melodic-move-base -y
+sudo apt-get install ros-melodic-random-numbers -y
+sudo apt-get install ros-melodic-moveit-kinematics -y
+sudo apt-get install ros-melodic-sound-play -y
+sudo apt-get install ros-melodic-dwa-local-planner
+sudo apt-get install swig
+sudo apt-get install python-pip
+sudo pip install pyaudio
+sudo pip install pocketsphinx
